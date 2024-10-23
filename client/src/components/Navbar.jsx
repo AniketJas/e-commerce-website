0aaa35e7b08ac021b1c9b20ec85747b9fabe0b7e
Aniket Jas <aniket.jas20@gmail.com>
2024-10-23 11:22:14 +0530

    Navbar us and branding

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,17 +9,18 @@ export const Navbar = ({ register = true, login = true }) => {
   }
 
   return (
-    <div className='w-full h-16 flex items-center px-10 border border-bottom-1 border-b-slate-500 shadow-md'>
+    <div className='w-full h-16 flex items-center px-10 border border-bottom-1 border-b-slate-300 shadow-md'>
       <div className='w-5/6'>
-        <button onClick={() => setRedirect("/")}>Homepage</button>
+        <button className='font-extrabold text-2xl' onClick={() => setRedirect("/")}>ShopSprint</button>
       </div>
-      <div className='w-1/6 flex justify-center'>
+      <div className='w-1/6 flex justify-end gap-6 font-semibold'>
         {login && (
-          <div className='w-1/2 flex justify-center'><button onClick={() => setRedirect("/login")}>Login</button></div>
+          <div className='flex justify-center'>
+            <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg" onClick={() => setRedirect("/login")}>Login</button></div>
         )}
         {register && (
-          <div className='w-1/2 flex justify-center'>
-            <button onClick={() => setRedirect("/register")}>Register</button>
+          <div className='flex justify-center'>
+            <button className="bg-teal-600 text-white px-4 py-2 rounded-lg" onClick={() => setRedirect("/register")}>Register</button>
           </div>
         )}
       </div>
